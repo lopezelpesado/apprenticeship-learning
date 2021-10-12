5bb30f48fc33bc4b010b8ff0da6b469df697e56d
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-12 13:05:04 +0100

    session auth part the first and part the second

--- week-3-auth/session-auth/workshop/auth.js
+++ week-3-auth/session-auth/workshop/auth.js
@@ -3,10 +3,33 @@ const bcrypt = require("bcryptjs");
 const model = require("./database/model");
 
 const COOKIE_OPTIONS = {
-  httpOnly: true,
-  maxAge: 600000,
-  sameSite: "strict",
-  signed: true,
+	httpOnly: true,
+	maxAge: 600000,
+	sameSite: "strict",
+	signed: true,
 };
 
-module.exports = { COOKIE_OPTIONS };
+async function createUser(email, password, name) {
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	return await model.createUser(email, hashedPassword, name);
+}
+
+async function saveUserSession(user) {
+	const sid = crypto.randomBytes(18).toString("base64");
+
+	return model.createSession(sid, { user });
+}
+
+// function createUser(email, password, name) {
+// 	return bcrypt
+// 		.hash(password, 10)
+// 		.then((hash) => model.createUser(email, hash, name));
+// }
+
+// function saveUserSession(user) {
+// 	const sid = crypto.randomBytes(18).toString("base64");
+// 	return model.createSession(sid, { user });
+// }
+
+module.exports = { createUser, saveUserSession, COOKIE_OPTIONS };
--- week-3-auth/session-auth/workshop/routes/signUp.js
+++ week-3-auth/session-auth/workshop/routes/signUp.js
@@ -1,7 +1,7 @@
 const auth = require("../auth.js");
 
 function get(request, response) {
-  response.send(`
+	response.send(`
     <h1>Create an account</h1>
     <form action="sign-up" method="POST">
       <label for="name">Name</label>
@@ -15,10 +15,29 @@ function get(request, response) {
   `);
 }
 
-function post(request, response) {
-  const { email, password, name } = request.body;
-  console.log("Signing up...");
-  response.redirect("/");
+async function post(request, response) {
+	const { email, password, name } = request.body;
+	console.log("Signing up...");
+	const user = await auth.createUser(email, password, name);
+	console.log(user);
+	const sid = await auth.saveUserSession(user);
+	console.log(sid);
+	response.cookie("sid", sid, auth.COOKIE_OPTIONS);
+	console.log(auth.COOKIE_OPTIONS);
+	console.log("redirecting");
+	response.redirect("/");
 }
+
+// function post(request, response) {
+// 	const { email, password, name } = request.body;
+// 	auth
+// 		.createUser(email, password, name)
+// 		.then(auth.saveUserSession)
+// 		.then((sid) => {
+// 			response.cookie("sid", sid, auth.COOKIE_OPTIONS);
+// 			response.redirect("/");
+// 		});
+// 	// ...
+// }
 
 module.exports = { get, post };
