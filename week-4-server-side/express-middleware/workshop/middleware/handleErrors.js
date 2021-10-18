47fe867bb1e1b968ef9ced832e5eb8bb674b0562
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-18 18:01:05 +0100

    modularised handle error func

--- week-4-server-side/express-middleware/workshop/middleware/handleErrors.js
+++ week-4-server-side/express-middleware/workshop/middleware/handleErrors.js
@@ -1,7 +1,14 @@
+const { STATUS_CODES } = require("http");
+
 function handleErrors(error, req, res, next) {
 	console.log(error);
 	const status = error.status || 500;
-	res.status(status).send(/*html*/ `<h1>Oh god, you broke it</h1>`);
+	if (process.env.NODE_ENV === "production") {
+		// if (true) {
+		res.status(status).send(/*html*/ `<h1>${STATUS_CODES[status]}</h1>`);
+	} else {
+		res.status(status).send(/*html*/ `<pre>${error.stack}</pre>`);
+	}
 }
 
 module.exports = handleErrors;
