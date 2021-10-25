6550080a18aeaba213469a96692e2ba16304be82
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-25 16:34:21 +0100

    react challenge 1 complete

---
+++ week5-client-side/dom-rendering/workshop/tempaltes/dogListTemplate.js
@@ -0,0 +1,7 @@
+const dogListTemplate = document.createElement("template");
+dogListTemplate.innerHTML = /* html */ `
+        <h1>All the dogs</h1>
+        <ul></ul>
+    `;
+
+export default dogListTemplate;
