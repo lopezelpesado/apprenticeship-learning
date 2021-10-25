f0c5b54c883f37c1078ef38857d98afccef049b3
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-25 12:09:59 +0100

    challenge 2 complete

--- week5-client-side/dom-rendering/workshop/app.js
+++ week5-client-side/dom-rendering/workshop/app.js
@@ -1,23 +1,37 @@
+import createEl from "./create-element.js";
 import dogData from "./dogs.js";
 
 const dogListItems = dogData.map((dog) => {
-	const dogName = document.createElement("h2");
-	dogName.textContent = dog.name;
+	// const dogName = document.createElement("h2");
+	// dogName.textContent = dog.name;
 
-	const dogPic = document.createElement("img");
-	dogPic.src = dog.image;
-	dogPic.alt = `Picture of ${dog.name}`;
+	const dogName = createEl("h2", { textContent: dog.name });
 
-	const dogListItem = document.createElement("li");
-	dogListItem.append(dogName, dogPic);
+	// const dogPic = document.createElement("img");
+	// dogPic.src = dog.image;
+	// dogPic.alt = `Picture of ${dog.name}`;
+
+	const dogPic = createEl("img", {
+		src: dog.image,
+		alt: `Picture of ${dog.name}`,
+	});
+
+	// const dogListItem = document.createElement("li");
+	// dogListItem.append(dogName, dogPic);
+
+	const dogListItem = createEl("li", {}, dogName, dogPic);
 
 	return dogListItem;
 });
 
-const title = document.createElement("h1");
-title.textContent = "All the dogs";
+// const title = document.createElement("h1");
+// title.textContent = "All the dogs";
 
-const dogList = document.createElement("ul");
-dogList.append(...dogListItems);
+const title = createEl("h1", { textContent: "All the dogs" });
+
+// const dogList = document.createElement("ul");
+// dogList.append(...dogListItems);
+
+const dogList = createEl("ul", {}, ...dogListItems);
 
 document.getElementById("app").append(title, dogList);
