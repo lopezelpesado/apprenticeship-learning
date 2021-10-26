001393d6607990ff82e8cabe1e1465acb2640cfc
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-26 10:28:32 +0100

    part 3 complete

--- week5-client-side/react-forms/workshop/App.jsx
+++ week5-client-side/react-forms/workshop/App.jsx
@@ -13,11 +13,44 @@ import dishes from "../data";
 // ];
 
 function App() {
+	const [min, setMin] = React.useState(0);
+	const [max, setMax] = React.useState(0);
+
 	return (
 		<main>
 			<section className='filters'>
 				<h1>Burger Place</h1>
-				<form>Inputs go here</form>
+				<form>
+					<fieldset>
+						<legend>Price</legend>
+						<label htmlFor='min-price'>
+							Min Price
+							<input
+								type='range'
+								name='min-price'
+								id='min-price'
+								min='0.5'
+								max='9'
+								step='0.25'
+								value={min}
+								onChange={(event) => setMin(event.target.value)}
+							/>
+						</label>
+						<label htmlFor='max-price'>
+							Max Price
+							<input
+								type='range'
+								name='max-price'
+								id='max-price'
+								min='0.5'
+								max='9'
+								step='0.25'
+								value={max}
+								onChange={(event) => setMax(event.target.value)}
+							/>
+						</label>
+					</fieldset>
+				</form>
 			</section>
 			<section className='dishes'>
 				<h2>Dishes</h2>
