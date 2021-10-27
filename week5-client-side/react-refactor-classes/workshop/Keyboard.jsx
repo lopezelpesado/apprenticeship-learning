6cc9ddace6ba253efca1bc9eef983cff188d97c5
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-27 10:44:07 +0100

    react refactor task 5

--- week5-client-side/react-refactor-classes/workshop/Keyboard.jsx
+++ week5-client-side/react-refactor-classes/workshop/Keyboard.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 
-class Keyboard extends React.Component {
-  state = {
-    key: "",
-  };
-  handleKeyDown = (event) => {
-    this.setState({ key: event.key });
-  };
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    // stops the event listener continuing to fire
-    // when the component has left the page
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-  render() {
-    return <div>{this.state.key || "Press any key"}</div>;
-  }
+function Keyboard() {
+	const [key, setKey] = React.useState(null);
+
+	React.useEffect(() => {
+		const handleKeyDown = (event) => {
+			setKey(event.key);
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+
+		const cleanup = () => window.removeEventListener("keydown", handleKeyDown);
+
+		return cleanup;
+	}, []);
+
+	return <div>{key || "Press any key"}</div>;
 }
+
+// class Keyboard extends React.Component {
+// 	state = {
+// 		key: "",
+// 	};
+// 	handleKeyDown = (event) => {
+// 		this.setState({ key: event.key });
+// 	};
+// 	componentDidMount() {
+// 		window.addEventListener("keydown", this.handleKeyDown);
+// 	}
+// 	componentWillUnmount() {
+// 		// stops the event listener continuing to fire
+// 		// when the component has left the page
+// 		window.removeEventListener("keydown", this.handleKeyDown);
+// 	}
+// 	render() {
+// 		return <div>{this.state.key || "Press any key"}</div>;
+// 	}
+// }
 
 export default Keyboard;
